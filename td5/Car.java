f690ea65df7db5d590b24e6846be99e16eb4a5d6
Johana L <johana.labou@dauphine.eu>
2021-12-07 11:05:19 +0100

    ajout corrections

--- td5/Car.java
+++ td5/Car.java
@@ -1,31 +1,81 @@
 package td5;
 
-public class Car {
-   private final String brand;
-   private final long value;
+import java.util.Objects;
 
+public class Car extends Vehicule {
 
-   public Car(String brand, long value){
-       this.brand = brand;
-       if (value<0){ throw new IllegalArgumentException();}
-       else{
-           this.value = value;
-       }
-   }
+    private final int value;
+    private final int vetuste;
+    private final String licencePlate;
 
-    public String getBrand() {
-        return brand;
+    public Car(String brand, String licencePlate, int value) {
+        this(brand, licencePlate, value, 0);
     }
 
-    public Car(long value) {
-        this.value = value;
+    public Car(String brand, String licencePlate, int value, int collection) {
+        this(brand, licencePlate, value, collection, null);
+    }
+
+    public Car(String brand, String licencePlate, int value, Discount d) {
+        this(brand, licencePlate, value, 0, d);
+    }
+
+    public Car(String brand, String licencePlate, int value, int collection, Discount d) {
+        super(brand, d);
+        if (brand == null)
+            throw new NullPointerException();
+        this.licencePlate = Objects.requireNonNull(licencePlate);
+        if (value < 0)
+            throw new IllegalArgumentException("negative value");
+        int computedValue = value - collection * 1000;
+        if (computedValue * 2 < value)
+            throw new IllegalArgumentException("too old");
+        this.value = computedValue;
+        this.vetuste = collection;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
     }
 
     @Override
     public String toString() {
-        return "Car{" +
-                "brand='" + brand + '\'' +
-                ", value=" + value +
-                '}';
+        return "Voiture " + getBrand() + " " + value;
+    }
+
+    public boolean equals(Car c) {
+        // primitif first
+        return value == c.value && getBrand().equals(c.getBrand()) && c.getLicencePlate().equals(licencePlate);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Car))
+            return false;
+        return equals((Car) obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return 42;
+    }
+
+
+    /**
+     * The second level comparison of elements,
+     * if they are of the same type (if condition is false)
+     * check equality of their content.
+     */
+
+    public int compareTo(Object o) {
+        if (!(o instanceof Car)) {
+            return super.compareTo(o);
+        }
+        Car c = (Car) o;
+        return (getBrand()+licencePlate+getValue()+vetuste).compareTo(c.getBrand()+c.licencePlate+c.getValue()+c.vetuste);
     }
 }
---
+++ td5/Discount.java
@@ -0,0 +1,13 @@
+package td5;
+
+public class Discount {
+    private final int val;
+
+    public Discount(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+}
